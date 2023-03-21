e47f9be76d7d751a15ccb62f2c84e04423dac124
unknown <44150851+aryasatya05@users.noreply.github.com>
2023-03-21 16:12:40 +0700

    penjumalahan commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+public class Penjumlahan
+{
+    public T JumlahTigaAngka<T>(T iSatu, T iDua, T iTiga)
+    {
+       return ((dynamic)iSatu + (dynamic)iDua + (dynamic)iTiga);
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Penjumlahan penjumlahan= new Penjumlahan();
+        Console.WriteLine(penjumlahan.JumlahTigaAngka<float>(13, 2, 21));
+
+    }
+}
